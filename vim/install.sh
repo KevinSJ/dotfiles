874fbd3e39620faa7c4882cfdbbfbd2413a23a31
Kevin <kevin4313@163.com>
2021-03-27 23:57:37 +1300

    Vim install update again.

--- vim/install.sh
+++ vim/install.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim_config_file=~/.config/nvim
 
 mkdir -p ~/.config/
 
 if [ ! -L "$nvim_config_file" ]; then
+  curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
   ln -s ~/.dotfiles/vim/vim.symlink "$nvim_config_file"
