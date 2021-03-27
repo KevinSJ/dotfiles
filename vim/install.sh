26ceb79da45e884c08bb01863ee48fee9ab62705
Kevin <kevin4313@163.com>
2021-03-27 23:52:46 +1300

    Update vim install file

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim_config_file=~/.config/nvim
 
 mkdir -p ~/.config/
