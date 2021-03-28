305aaa634d704b699bede5431956021d94123905
Kevin <kevin4313@163.com>
2021-03-28 15:21:51 +1300

    Update polybar and bspwm configurations

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,11 +2,20 @@
 
 killall -q polybar
 
+export BAT=$(ls -A1 /sys/class/power_supply|grep -E 'BAT')
+export AC=$(ls -A1 /sys/class/power_supply|grep -v 'BAT')
+export ETH_INTERFACE=$(ip -4 -o link show | awk -F': ' '{print $2}'|grep en)
+export WLAN_INTERFACE=$(ip -4 -o link show | awk -F': ' '{print $2}'|grep wl)
+
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-
-polybar top > /dev/null 2>&1 &
-#polybar bottom &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload top &
+  done
+else
+  polybar --reload top &
+fi
 
 
 echo "Bar launched..."
