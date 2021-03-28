dec239be8fedd07b7555abc7ccac7ac5350fe458
Kevin Jiang <kevin4313@163.com>
2021-03-28 17:08:18 +1300

    Update vim install script

--- vim/install.sh
+++ vim/install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 nvim_config_file=~/.config/nvim
+DOTFILES_ROOT=$(pwd -P)
 
 mkdir -p ~/.config/
 
@@ -8,5 +9,5 @@ if [ ! -L "$nvim_config_file" ]; then
       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-  ln -s ~/.dotfiles/vim/vim.symlink "$nvim_config_file"
+  ln -s $DOTFILES_ROOT/vim/vim.symlink "$nvim_config_file"
 fi
