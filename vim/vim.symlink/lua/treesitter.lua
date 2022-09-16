aa29a41b6719dc8f23a9977f670c17a37a326a14
Sicong Jiang <sicong.jiang@spark.co.nz>
2022-09-16 18:59:35 +1200

    [ neovim ] Migrated treesitter configuration to separate file

---
+++ vim/vim.symlink/lua/treesitter.lua
@@ -0,0 +1,70 @@
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = "all",
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- List of parsers to ignore installing (for "all")
+  ignore_install = { "phpdoc", "tree-sitter-phpdoc" },
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+    -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+    -- the name of the parser)
+    -- list of language that will be disabled
+    disable = { "phpdoc", "tree-sitter-phpdoc" }, -- list of language that will be disabled
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+
+  context_commentstring = {
+    enable = true,
+  },
+
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = "gss",
+      node_incremental = "gsi",
+      node_decremental = "gsd",
+    },
+  },
+
+  textobjects = {
+      select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+              ["ac"] = "@class.outer",
+              ["ic"] = "@class.inner",
+              ["af"] = "@function.outer",
+              ["if"] = "@function.inner",
+              ["aa"] = "@parameter.outer",
+              ["ia"] = "@parameter.inner",
+          }
+      },
+  },
+}
+
+require'treesitter-context'.setup{
+    enable = true,
+    patterns = {
+        default = {
+            'class',
+            'function',
+            'method',
+        },
+        terraform = {
+            'block',
+        }
+    },
+    mode = 'topline',
+}
--- vim/vim.symlink/settings.lua
+++ vim/vim.symlink/settings.lua
@@ -1,27 +1,5 @@
 local home    = os.getenv("HOME")
 
--- TO BE MIGRATED TO SEPARATE FILE
-require'nvim-treesitter.configs'.setup {
-    ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    highlight = {
-        enable = true,              -- false will disable the whole extension
-        disable = { "c", "rust", "go", "c_sharp" },  -- list of language that will be disabled
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-    },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            init_selection = "<CR>",
-            scope_incremental = "<CR>",
-            node_incremental = "<TAB>",
-            node_decremental = "<S-TAB>"
-        }
-    }
-}
 vim.g.loaded_gzip              = 1
 vim.g.loaded_tar               = 1
 vim.g.loaded_tarPlugin         = 1
