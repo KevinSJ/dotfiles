461fbab5fbbcb061774da46462a23b0bebf8d91c
Kevin Jiang <KevinSJ@users.noreply.github.com>
2026-01-23 23:55:00 +1300

    feat: lspconfig

--- vim/nvim.symlink/lua/lsp/lspconfig.lua
+++ vim/nvim.symlink/lua/lsp/lspconfig.lua
@@ -7,6 +7,10 @@
 
 -- For configuration see the Wiki: https://github.com/neovim/nvim-lspconfig/wiki
 -- Autocompletion settings of "nvim-cmp" are defined in plugins/nvim-cmp.lua
+local home = os.getenv "HOME"
+
+lspconfig = vim.lsp.config
+enable = vim.lsp.enable
 
 local cmp_status_ok, cmp_nvim_lsp = pcall(require, 'cmp_nvim_lsp')
 if not cmp_status_ok then
@@ -32,38 +36,39 @@ local on_attach = function(client, bufnr)
   -- See: https://sbulav.github.io/til/til-neovim-highlight-references/
   -- for the highlight trigger time see: `vim.opt.updatetime`
   if client.server_capabilities.documentHighlightProvider then
-      vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
-      vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
-      vim.api.nvim_create_autocmd('LspNotify', {
-          callback = function(args)
-            if args.data.method == 'textDocument/didOpen' then
-              vim.lsp.foldclose('imports', vim.fn.bufwinid(args.buf))
-            end
-          end,
-          desc = "Auto fold all imports"
-        })
-      vim.api.nvim_create_autocmd("CursorHold", {
-          callback = vim.lsp.buf.document_highlight,
-          buffer = bufnr,
-          group = "lsp_document_highlight",
-          desc = "Document Highlight",
-      })
-      vim.api.nvim_create_autocmd("CursorMoved", {
-          callback = vim.lsp.buf.clear_references,
-          buffer = bufnr,
-          group = "lsp_document_highlight",
-          desc = "Clear All the References",
-      })
+    vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
+    vim.api.nvim_clear_autocmds { buffer = bufnr, group = "lsp_document_highlight" }
+    vim.api.nvim_create_autocmd('LspNotify', {
+      callback = function(args)
+        if args.data.method == 'textDocument/didOpen' then
+          vim.lsp.foldclose('imports', vim.fn.bufwinid(args.buf))
+        end
+      end,
+      desc = "Auto fold all imports"
+    })
+    vim.api.nvim_create_autocmd("CursorHold", {
+      callback = vim.lsp.buf.document_highlight,
+      buffer = bufnr,
+      group = "lsp_document_highlight",
+      desc = "Document Highlight",
+    })
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      callback = vim.lsp.buf.clear_references,
+      buffer = bufnr,
+      group = "lsp_document_highlight",
+      desc = "Clear All the References",
+    })
   end
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
   vim.keymap.set('n', '<leader>k', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  -- DISABLED BEACAUSE IT CONFLICTS WITH TMUX NAVIGATION
+  --vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
   vim.keymap.set('n', '<leader>wl', function()
@@ -88,7 +93,7 @@ vim.diagnostic.config({
     source = "always",
     header = "",
     prefix = "",
-	},
+  },
 })
 
 -- Show line diagnostics automatically in hover window
@@ -98,7 +103,7 @@ vim.cmd([[
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap=true, silent=true }
+local opts = { noremap = true, silent = true }
 vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
@@ -123,12 +128,119 @@ Java/Kotlin          -> jdtls
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches.
 -- Add your language server below:
-local servers = { 'bashls', 'basedpyright', 'clangd', 'jdtls', 'gopls', 'ts_ls'}
+local servers = { 'bashls', 'lua_ls', 'jdtls', 'ts_ls', 'eslint', 'pyright', 'clangd', 'html',
+  'cssls' }
 
---vim.lsp.config('jdtls', {
+local jar_dir = home .. "/jars/lombok.jar"
+
+require('spring_boot').init_lsp_commands()
+
 vim.lsp.config['jdtls'] = {
-  handlers = handlers,
-  cmd = {"jdtls"}
+  cmd = {
+    "jdtls",
+    "--no-validate-java-version",
+  },
+  flags = {
+    allow_incremental_sync = true,
+  },
+  handlers = {
+    ["language/status"] = function() end,
+  },
+  --[[
+     [cmd = {
+     [  'jdtls',
+     [  '--java-executable',
+     [  '/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home/bin/java',
+     [  '--jvm-arg=-javaagent:' .. jar_dir,
+     [},
+     ]]
+  detached = true,
+
+  -- `root_dir` must point to the root of your project.
+  -- See `:help vim.fs.root`
+  --root_markers = vim.fs.root(0, { 'gradlew', '.git', 'mvnw' }),
+  root_markers = { { "mvnw", "gradlew", "settings.gradle", "settings.gradle.kts", ".git" }, { "build.xml", "pom.xml", "build.gradle", "build.gradle.kts" } },
+  filetypes = { 'java', 'kotlin' },
+  --root_dir = root_dir,
+  capabilities = capabilities,
+  on_attach = on_attach,
+  init_options = {
+    bundles = require("spring_boot").java_extensions(),
+  },
+  settings = {
+    java = {
+      --home = "/Library/Java/JavaVirtualMachines/temurin-21.jdk/Contents/Home",
+      --home = "/usr/lib/jvm/default-runtime",
+      home = "/usr/lib/jvm/java-21-openjdk",
+      eclipse = {
+        downloadSources = true,
+      },
+      imports = {
+        gradle = {
+          wrapper = {
+            checksums = { 'f86344275d1b194688dd330abf9f6f2344cd02872ffee035f2d1ea2fd60cf7f3' }
+          }
+        }
+      },
+      import = {
+        gradle = {
+          enabled = false,
+          wrapper = {
+            enabled = true
+          },
+          offline = {
+            enabled = true
+          },
+          annotationProcessing = {
+            enabled = true
+          }
+        },
+        maven = {
+          enabled = true
+        }
+      },
+      gradle = {
+        enabled = true,
+        wrapper = {
+          enabled = true
+        },
+        offline = {
+          enabled = true
+        }
+      },
+      maven = {
+        downloadSources = true,
+      },
+      implementationsCodeLens = {
+        enabled = true,
+      },
+      referencesCodeLens = {
+        enabled = true,
+      },
+      references = {
+        includeDecompiledSources = true,
+      },
+      inlayHints = {
+        parameterNames = {
+          enabled = "all", -- literals, all, none
+        },
+      },
+      configuration = {
+        updateBuildConfiguration = "interactive",
+        runtimes = {
+          {
+            name = "JavaSE-21",
+            path = "/usr/lib/jvm/java-21-openjdk",
+            default = true
+          },
+          {
+            name = "JavaSE-17",
+            path = "/usr/lib/jvm/java-17-openjdk",
+          },
+        },
+      },
+    },
+  },
 }
 
 vim.lsp.config['ts_ls'] = {
@@ -148,7 +260,6 @@ vim.lsp.config['ts_ls'] = {
       description = "Remove unused",
     },
   },
-
   completions = {
     completeFunctionCalls = true,
   },
@@ -179,13 +290,27 @@ vim.lsp.config['ts_ls'] = {
   },
 }
 
+
+
+vim.lsp.config['eslint'] = {
+  on_attach = function(client, bufnr)
+    -- you can add other stuff here e.g. keymaps
+    on_attach(client, bufnr)
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      buffer = bufnr,
+      command = "EslintFixAll",
+    })
+  end,
+  root_dir = root_dir,
+  capabilities = capabilities,
+}
+
+
 -- Call setup
 for _, lsp in ipairs(servers) do
   vim.lsp.config(lsp, {
     on_attach = on_attach,
     --root_dir = root_dir,
-    --filetypes = filetypes,
-    --root_markers = { '.git' },
     capabilities = capabilities,
   })
 end
