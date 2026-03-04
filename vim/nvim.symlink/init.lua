740b050d7e06568e6872921117eac61bb2810482
Kevin Jiang <KevinSJ@users.noreply.github.com>
2026-03-04 23:01:36 +1300

    feat(nvim): update require

--- vim/nvim.symlink/init.lua
+++ vim/nvim.symlink/init.lua
@@ -6,21 +6,16 @@ Website: https://github.com/brainfucksec/neovim-lua
 
 --]]
 
-vim.opt.runtimepath:remove(vim.fn.expand('~/.config/nvim/'))
-vim.opt.runtimepath:remove(vim.fn.expand('~/dotfiles/'))
-vim.opt.runtimepath:remove(vim.fn.expand('~/.vim/'))
-vim.opt.packpath:remove(vim.fn.expand('~/.local/share/nvim/site'))
 -- Change leader to space
 vim.g.mapleader = ' '
+--vim.lsp.set_log_level("debug")
 
 -- Import Lua modules
-vim.api.nvim_command('let &packpath = &runtimepath')
-vim.api.nvim_command('set runtimepath^=~/Private/neovim-lua/nvim/')
 require('core/lazy')
-vim.api.nvim_command('set runtimepath^=~/Private/neovim-lua/nvim/')
 require('core/autocmds')
 require('core/colors')
 require('core/statusline')
+require('core/functions')
 require('core/options')
 require('lsp/lspconfig')
 require('plugins/init')
